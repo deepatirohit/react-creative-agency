0100fd90563401830cf1818a7feed27e3c93c5b0
Rohit Deepati <rohit.deepati@hexanovamedia.tech>
2025-06-26 21:11:07 +0530

    fix the issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { AdvertisementSection } from "./Components/AdvertisementSection/AdSection";
+import { DesignImages } from "./Components/DesignImages";
 import { FeaturesSection } from "./Components/FeatureSection/FeaturesSection";
+import { Footer } from "./Components/Footer/Footer";
 import { HomePage } from "./Components/HomePage/Home";
+import { LogoCarousel } from "./Components/LogoCarousel/LogoCarousel";
+import { MotivationSection } from "./Components/Motivation/MotivationComponent";
 import { NavBar } from "./Components/Navbar/Nav";
+import { PortfolioSection } from "./Components/PortfolioSection";
+import { ProgressSection } from "./Components/Progress/Progress";
 
 function App() {
   return (
@@ -8,6 +15,13 @@ function App() {
       <NavBar />
       <HomePage />
       <FeaturesSection />
+      <PortfolioSection />
+      <DesignImages />
+      <ProgressSection />
+      <MotivationSection />
+      <AdvertisementSection />
+      <LogoCarousel />
+      <Footer />
     </>
   );
 }
